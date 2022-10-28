fa262e37d3ede64563d207f42a011e558f5f8ee1
ALTR <chuan.khuna@gmail.com>
2022-10-28 13:41:05 +0700

    update utils to save topics

--- utils/scraper/pantip/utils.py
+++ utils/scraper/pantip/utils.py
@@ -12,3 +12,15 @@ def save_comments_json(scrape_result: list[dict], output_folder: str = "."):
     output_file = f"{output_folder}/{topic_id}.json"
     with open(output_file, "w") as f:
         f.write(json.dumps(comments, ensure_ascii=False, indent=2))
+
+
+def save_topics_json(keyword: str, scrape_result: list[dict], output_folder: str = "."):
+    topics = []
+
+    for batch in scrape_result:
+        if 'data' in batch.keys():
+            topics += batch['data']
+
+    output_file = f"{output_folder}/{keyword}.json"
+    with open(output_file, "w") as f:
+        f.write(json.dumps(topics, ensure_ascii=False, indent=2))
