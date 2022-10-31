3c92a99a1af810ae374ee648de54adf744b51d40
ALTR <chuan.khuna@gmail.com>
2022-10-31 18:10:49 +0700

    add multithread comments scraping helper

--- utils/scraper/pantip/helper.py
+++ utils/scraper/pantip/helper.py
@@ -5,14 +5,25 @@ import pandas as pd
 import numpy as np
 
 
-def scrape_all_topics(keyword: str, max_page: int = 50):
-    n_pages = get_number_of_topic_pages(keyword)
-    if n_pages > max_page:
-        n_pages = max_page
+def multithread_scrape_comments(scraper,
+                                topic_ids: list[str | int],
+                                save_path: str,
+                                pool_size: int = 4):
+    """Use multithreading(multiprocessing) to speed up scraping speed
+    sprape for comments of multiple topics simultaneously
 
-    data = []
-    for page in range(1, n_pages + 1):
-        res = scrape_topics(keyword, page)
-        data += res['data']
+    Args:
+        scraper (_type_): comment scrapers
+        topic_ids (list[str  |  int]): a list of topic ids
+        save_path (str): path to save comments json
+        pool_size (int, optional): CPU's core to use. Defaults to 4.
+    """
 
-    return data
+    # each element is a list of responses for a topic
+    with Pool(pool_size) as p:
+        responses = p.map(scraper.scrape, topic_ids)
+
+    folder_ = [save_path] * len(responses)
+
+    with Pool(pool_size) as p:
+        p.starmap(save_comments_json, zip(responses, folder_))
