0f6ca4bb60c8f73cd7a4172974351703faab7277
ALTR <chuan.khuna@gmail.com>
2022-11-08 14:54:37 +0700

    Update n-gram utils to drop stopwords combination

--- utils/preprocessing/text/ngram.py
+++ utils/preprocessing/text/ngram.py
@@ -13,6 +13,7 @@ class NGram:
         self.delimiter = '_'
         self.only_ngram = False
         self.remove_delim = True
+        self.stopwords: list[str] = []
 
     def fit(self, tokenised_texts: list[list[str]], **kwargs) -> dict[int, gensim.models.Phrases]:
         """train 2 to max_n-gram models
@@ -47,6 +48,16 @@ class NGram:
             n_gram = new_ngram
         return n_gram
 
+    def _filter_stopwords(self, n_gram: list[str]) -> list[str]:
+        non_stopwords_ngram = []
+        for ngram_word in n_gram:
+            all_stopword = True
+            for token in ngram_word.split(self.delimiter):
+                all_stopword = all_stopword and (token in self.stopwords)
+            if not all_stopword:
+                non_stopwords_ngram.append(ngram_word)
+        return non_stopwords_ngram
+
     def get_ngrams(self, tokenised_text: list[str]) -> dict[int, list[str]]:
         """get 2-gram(bigram) to max_n-gram
         set only_ngram=True to filter only n-gram, otherwise it will return all words with n-gram pairs
@@ -65,6 +76,7 @@ class NGram:
         for n, model in self.models.items():
             n_gram = model[prev_gram]
             prev_gram = n_gram
+            n_gram = self._filter_stopwords(n_gram)
             n_gram = self._filter_n_gram(n_gram, n)
             n_gram = self._remove_delimiter(n_gram)
             n_grams[n] = n_gram
