03daf307435d62786344d03a195334c0fd2f7cec
ALTR <chuan.khuna@gmail.com>
2022-11-08 13:41:35 +0700

    update wordcloud utils

--- utils/vis_utils/wordcloud.py
+++ utils/vis_utils/wordcloud.py
@@ -4,23 +4,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plot_wordcloud(tokenised_texts):
+def generate_wordcloud(tokenised_texts):
     words, counts = np.unique(np.concatenate(list(tokenised_texts)), return_counts=True)
     freq_dict = {}
     for k, v in zip(words, counts):
         freq_dict[k] = v
     wc = wordcloud.WordCloud(
         font_path="./fonts/Noto_Sans_Thai/NotoSansThai-VariableFont_wdth,wght.ttf",
-        width=1200,
-        height=800,
+        width=2000,
+        height=1600,
         max_words=200,
         prefer_horizontal=0.9,
         background_color='white',
         colormap='plasma')
     wc.generate_from_frequencies(freq_dict)
 
-    plt.imshow(wc)
-    plt.axis(False)
-    plt.show()
+    # plt.imshow(wc)
+    # plt.axis(False)
+    # plt.show()
 
     return wc
