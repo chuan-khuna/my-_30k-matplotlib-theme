4a0450c963412e8eba42845d8ed5583ae563fa1e
ALTR <chuan.khuna@gmail.com>
2022-11-19 14:39:00 +0700

    Update requests by using params

--- utils/scraper/twitter/tweet_scraper.py
+++ utils/scraper/twitter/tweet_scraper.py
@@ -11,6 +11,9 @@ class TweetScraper:
         self.api_url = "https://twitter.com/i/api/2/search/adaptive.json"
         self.headers = self._load_header_from_yaml(header_yaml_path)
         self.data_to_extract = ['tweets', 'users']
+        # default twitter search option is searching for top tweets
+        # it will reach lazyload limit ~20 pages (not sure)
+        self.search_latest_tweets = False
 
     def _load_header_from_yaml(self, yaml_path: str) -> dict:
         with open(yaml_path) as f:
@@ -29,35 +32,92 @@ class TweetScraper:
         keyword = re.sub(r"\s", "%20", keyword)
         return keyword
 
-    def _build_payload(self, keyword: str, cursor_token: str) -> str:
+    def _build_payload(self, keyword: str, cursor_token: str) -> dict:
         # see more: https://requests.readthedocs.io/en/latest/user/quickstart/
         # Passing Parameters In URLs (GET request)
+        # you can find the configuration by inspecting payload on twitter search page
+
+        params = {
+            'include_profile_interstitial_type':
+                1,
+            'include_blocking':
+                1,
+            'include_blocked_by':
+                1,
+            'include_followed_by':
+                1,
+            'include_want_retweets':
+                1,
+            'include_mute_edge':
+                1,
+            'include_can_dm':
+                1,
+            'include_can_media_tag':
+                1,
+            'include_ext_has_nft_avatar':
+                1,
+            'include_ext_is_blue_verified':
+                1,
+            'skip_status':
+                1,
+            'cards_platform':
+                'Web-12',
+            'include_cards':
+                1,
+            'include_ext_alt_text':
+                True,
+            'include_ext_limited_action_results':
+                False,
+            'include_quote_count':
+                True,
+            'include_reply_count':
+                1,
+            'tweet_mode':
+                'extended',
+            'include_ext_collab_control':
+                True,
+            'include_entities':
+                True,
+            'include_user_entities':
+                True,
+            'include_ext_media_color':
+                True,
+            'include_ext_media_availability':
+                True,
+            'include_ext_sensitive_media_warning':
+                True,
+            'include_ext_trusted_friends_metadata':
+                True,
+            'send_error_codes':
+                True,
+            'simple_quoted_tweet':
+                True,
+            # keyword to search here
+            'q':
+                'sawano hiroyuki',
+            'count':
+                20,
+            'query_source':
+                'typed_query',
+            'pc':
+                1,
+            'spelling_corrections':
+                1,
+            'include_ext_edit_control':
+                True,
+            'ext':
+                'mediaStats,highlightedLabel,hasNftAvatar,voiceInfo,enrichments,superFollowMetadata,unmentionInfo,editControl,collab_control,vibe'
+        }
+
+        params['q'] = keyword
 
         if cursor_token:
-            cursor_param = f"&cursor={cursor_token}"
-        else:
-            cursor_param = ''
+            params['cursor'] = cursor_token
 
-        keyword_param = self._format_keyword(keyword)
+        if self.search_latest_tweets == True:
+            params['tweet_search_mode'] = 'live'
 
-        payload = f"?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&include_ext_has_nft_avatar=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_ext_alt_text=true&include_ext_limited_action_results=false&include_quote_count=true&include_reply_count=1&tweet_mode=extended&include_ext_collab_control=true&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&include_ext_sensitive_media_warning=true&include_ext_trusted_friends_metadata=true&send_error_codes=true&simple_quoted_tweet=true&q={keyword_param}&count=20&query_source=typeahead_click{cursor_param}&pc=1&spelling_corrections=1&include_ext_edit_control=true&ext=mediaStats%2ChighlightedLabel%2ChasNftAvatar%2CreplyvotingDownvotePerspective%2CvoiceInfo%2Cenrichments%2CsuperFollowMetadata%2CunmentionInfo%2CeditControl%2Ccollab_control%2Cvibe"
-
-        return payload
-
-    def _create_request_url(self, keyword: str, cursor_token: str):
-        payload = self._build_payload(keyword, cursor_token)
-
-        # ensure that there is only one '?'
-        # only appears at a payload
-        if not payload.startswith('?'):
-            payload = '?' + payload
-
-        if self.api_url.endswith('?'):
-            api_url = self.api_url[:-1]
-        else:
-            api_url = self.api_url
-
-        return api_url + payload
+        return params
 
     def _extract_token(self, response: dict) -> str | None:
         """Extract cursor token from a raw response
@@ -146,9 +206,10 @@ class TweetScraper:
             dict: raw json response
         """
 
-        url = self._create_request_url(keyword, cursor_token)
+        params = self._build_payload(keyword, cursor_token)
+
         try:
-            res = requests.get(url, headers=self.headers)
+            res = requests.get(self.api_url, params=params, headers=self.headers, timeout=5)
 
             if res.status_code == 200:
                 response_json = res.json()
