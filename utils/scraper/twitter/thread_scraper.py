87c70c8748991e1b77fc5fa477355bf93a436171
ALTR <chuan.khuna@gmail.com>
2022-11-26 18:04:48 +0700

    add twitter scraper base class

--- utils/scraper/twitter/thread_scraper.py
+++ utils/scraper/twitter/thread_scraper.py
@@ -2,19 +2,15 @@ import json
 import yaml
 import requests
 import re
+from .twitter_scraper_base import TwitterScraperBase
 
 
-class ThreadScraper:
+class ThreadScraper(TwitterScraperBase):
 
     def __init__(self, header_yaml_path: str):
         self.max_lazyload = 10
         self.api_url = "https://twitter.com/i/api/graphql/BoHLKeBvibdYDiJON1oqTg/TweetDetail"
         self.headers = self._load_header_from_yaml(header_yaml_path)
-
-    def _load_header_from_yaml(self, yaml_path: str) -> dict:
-        with open(yaml_path) as f:
-            headers = yaml.load(f, yaml.Loader)
-        return headers
 
     def _build_payload(self, tweet_id: str, cursor_token: str) -> dict:
         params = {
