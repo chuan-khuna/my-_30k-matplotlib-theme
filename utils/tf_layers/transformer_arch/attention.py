0babf297ac508479e470536fd0174f26292f3526
ALTR <chuan.khuna@gmail.com>
2022-12-16 22:20:44 +0700

    add attention blocks

---
+++ utils/tf_layers/transformer_arch/attention.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+class BaseMultiHeadedAttentionBlock(keras.layers.Layer):
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.attention = keras.layers.MultiHeadAttention(**kwargs)
+        self.layernorm = keras.layers.LayerNormalization()
+        self.residual_add = keras.layers.Add()
+
+    def get_config(self):
+        config = super().get_config()
+        return config
+
+
+class SelfAttentionBlock(BaseMultiHeadedAttentionBlock):
+
+    def call(self, x):
+        attn_output = self.attention(query=x, key=x, value=x)
+        x = self.residual_add([x, attn_output])
+        x = self.layernorm(attn_output)
+        return x
+
+
+class MaskedSelfAttentionBlock(BaseMultiHeadedAttentionBlock):
+
+    def call(self, x):
+        attn_output = self.attention(query=x, key=x, value=x, use_causal_mask=True)
+        x = self.residual_add([x, attn_output])
+        x = self.layernorm(attn_output)
+        return x
+
+
+class CrossAttentionBlock(BaseMultiHeadedAttentionBlock):
+
+    def call(self, x, context):
+        attn_output, self.attn_scores = self.attention(query=x,
+                                                       key=context,
+                                                       value=context,
+                                                       return_attention_scores=True)
+        x = self.residual_add([x, attn_output])
+        x = self.layernorm(attn_output)
+        return x
