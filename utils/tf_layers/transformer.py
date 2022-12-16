01ceac3831162586b3e2666889fc6d182984d77b
ALTR <chuan.khuna@gmail.com>
2022-12-16 10:58:14 +0700

    add tensorflow snippet

---
+++ utils/tf_layers/transformer.py
@@ -0,0 +1,9 @@
+# reference
+# https://keras.io/examples/nlp/text_classification_with_transformer/
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+class TransformerBlock(keras.layers.Layer):
+    pass
