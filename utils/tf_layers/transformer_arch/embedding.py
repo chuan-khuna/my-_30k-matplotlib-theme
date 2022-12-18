46ca0a6a761c0f7672980f1037c0e7eef0ae8a6d
ALTR <chuan.khuna@gmail.com>
2022-12-18 11:34:17 +0700

    add note to set mask_zero in Embedding layer

--- utils/tf_layers/transformer_arch/embedding.py
+++ utils/tf_layers/transformer_arch/embedding.py
@@ -1,6 +1,11 @@
 # ref: https://machinelearningmastery.com/the-transformer-positional-encoding-layer-in-keras-part-2/
 # https://www.tensorflow.org/text/tutorials/transformer#the_embedding_and_positional_encoding_layer
 
+# don't forget to add mask_zero at the Embedding layer
+# preprocessing token to vector - https://www.tensorflow.org/api_docs/python/tf/keras/layers/TextVectorization
+# Embedding masking - https://www.tensorflow.org/text/tutorials/transformer#the_embedding_and_positional_encoding_layer
+# evaluation metrics - https://www.tensorflow.org/text/tutorials/transformer#set_up_the_loss_and_metrics
+
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -11,7 +16,7 @@ class FixedPositionalEncoding(keras.layers.Layer):
 
     How to use:
 
-    model.add(Embedding(...))
+    model.add(Embedding(..., mask_zero=True))
     model.add(FixedPositionalEncoding(....))
 
     Ref: Attention is All You Need (Vaswani et al)
